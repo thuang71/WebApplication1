b180fbea24cefb22e1be73cce6db1bf90e524193
thuang71 <79078920+thuang71@users.noreply.github.com>
2023-02-13 20:12:27 -0800

    Add project files.

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Diagnostics;
+
+
+namespace WebApplication1.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult PaymentIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProcessPayment(Payment payment)
+        {
+            // Authenticate user credentials
+
+            // Validate credit card
+
+            // Generate authorization code or decline message
+
+            return View();
+        }
+    }
+
+}
---
+++ Models/Payment.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class Payment
+    {
+
+        public string? CreditCardNumber { get; set; }
+        public string? ExpirationDate { get; set; }
+        public string? CVV { get; set; }
+        public decimal Amount { get; set; }
+
+    }
+}
